a8eeae742cc10d5feeebe8cc8006f9a7ddcfe7c0
gsmith660 <gsmith660@gmail.com>
2020-01-06 20:03:21 -0600

    Studio 1 Bonus 2 completed

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,19 +5,27 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean validInput = false;
+        Double radius = 0d;
 
-        System.out.print("Enter a radius: ");
-        if (input.hasNextDouble()) {
-            Double radius = input.nextDouble();
-            if (radius < 0) {
-                System.out.println("Error: The radius must be non-negative!");
+        while (!validInput) {
+            System.out.print("Enter a radius: ");
+            if (input.hasNextDouble()) {
+                radius = input.nextDouble();
+                if (radius < 0) {
+                    System.out.println("Error: The radius must be non-negative!");
+                } else {
+                    validInput = true;
+                }
             } else {
-                Double area = Circle.getArea(radius);
-                System.out.println("The area of a circle of radius " + radius + " is: " + area);
+                System.out.println("Error: You must enter a number!");
+                input.next();
             }
-        } else {
-            System.out.println("Error: You must enter a number!");
         }
-        
+        input.close();
+
+        Double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+
     }
 }
