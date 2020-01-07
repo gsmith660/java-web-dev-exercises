aad470f554adb664ead1a6ada33712ac4138483f
gsmith660 <gsmith660@gmail.com>
2020-01-06 20:30:05 -0600

    Studio 1 Bonus 2 alternate method implemented

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,23 +5,37 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean validInput = false;
-        Double radius = 0d;
 
-        while (!validInput) {
+        double radius;
+
+        do {
             System.out.print("Enter a radius: ");
-            if (input.hasNextDouble()) {
-                radius = input.nextDouble();
-                if (radius < 0) {
-                    System.out.println("Error: The radius must be non-negative!");
-                } else {
-                    validInput = true;
-                }
-            } else {
+            while (!input.hasNextDouble()) {
                 System.out.println("Error: You must enter a number!");
                 input.next();
             }
-        }
+            radius = input.nextDouble();
+            if (radius < 0) {
+                System.out.println("Error: The radius must be non-negative!");
+            }
+        } while (radius < 0);
+
+//        boolean validInput = false;
+//        while (!validInput) {
+//            System.out.print("Enter a radius: ");
+//            if (input.hasNextDouble()) {
+//                radius = input.nextDouble();
+//                if (radius < 0) {
+//                    System.out.println("Error: The radius must be non-negative!");
+//                } else {
+//                    validInput = true;
+//                }
+//            } else {
+//                System.out.println("Error: You must enter a number!");
+//                input.next();
+//            }
+//        }
+
         input.close();
 
         Double area = Circle.getArea(radius);
