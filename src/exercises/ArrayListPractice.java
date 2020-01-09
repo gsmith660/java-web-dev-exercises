95f7ec0289fb261070a90e8c313e2ef1df90320e
gsmith660 <gsmith660@gmail.com>
2020-01-08 18:53:23 -0600

    replaced specific types ArrayList and HashMap with generic interfaces List and Map, but still instantiated as ArrayList and HashMap

--- src/exercises/ArrayListPractice.java
+++ src/exercises/ArrayListPractice.java
@@ -2,21 +2,21 @@ package exercises;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 public class ArrayListPractice {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 11; i++) {
             numbers.add(i);
         }
         System.out.println(sumEven(numbers));
 
-        ArrayList<String> words = new ArrayList<>(Arrays.asList(ArrayPractice.seuss.split(" |, |\\. |\\.")));
+        List<String> words = new ArrayList<>(Arrays.asList(ArrayPractice.seuss.split(" |, |\\. |\\.")));
         printWordsOfFixedLength(words, 5);
     }
 
-    private static int sumEven(ArrayList<Integer> list) {
+    private static int sumEven(List<Integer> list) {
         int sum = 0;
         for (int num : list) {
             if (num % 2 == 0) {
@@ -26,7 +26,7 @@ public class ArrayListPractice {
         return sum;
     }
 
-    private static void printWordsOfFixedLength(ArrayList<String> words, int length) {
+    private static void printWordsOfFixedLength(List<String> words, int length) {
         for (String word : words) {
             if (word.length() == length) {
                 System.out.println(word);
--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class HashMapPractice {
     public static void main(String[] args) {
-        HashMap<Integer, String> students = new HashMap<>();
+        Map<Integer, String> students = new HashMap<>();
         Scanner input = new Scanner(System.in);
         String newStudent;
 
