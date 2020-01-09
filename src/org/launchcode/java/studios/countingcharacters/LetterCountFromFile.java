e6810289e4432b956b897b4bd10b95a7b7b84401
gsmith660 <gsmith660@gmail.com>
2020-01-08 19:36:06 -0600

    Studio 2 Super Bonus added read from file line by line

---
+++ src/org/launchcode/java/studios/countingcharacters/LetterCountFromFile.java
@@ -0,0 +1,37 @@
+package org.launchcode.java.studios.countingcharacters;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
+public class LetterCountFromFile {
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(LetterCountFromFile.class.getResourceAsStream("inputText.txt"));
+        List<String> fileLines = new ArrayList<String>();
+        while (in.hasNextLine()) {
+            fileLines.add(in.nextLine());
+        }
+        in.close();
+        String frequencyCountString = String.join("\n", fileLines);
+
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : frequencyCountString.toCharArray()) {
+            if (Character.isLetter(c)) {
+                c = Character.toUpperCase(c);
+                if (counts.containsKey(c)) {
+                    counts.replace(c, counts.get(c) + 1);
+                } else {
+                    counts.put(c, 1);
+                }
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : counts.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
+}
